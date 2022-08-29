6a86e19698b180da6da855fc9df9b1287a30778f
Henrik Palmlund Wahlgren <henrik@pwit.se>
2022-08-29 09:37:05 +0200

    added history and summations

--- scripts/get_spot_prices.py
+++ scripts/get_spot_prices.py
@@ -74,8 +74,36 @@ def save_day_to_file(
     with open(f"{BASE_DIRECTORY}/{price_area}/{year}/{month}/{day}/index.json", "w") as file:
         json.dump(output, file)
 
+def save_month_to_file(
+    month_string: str, hourly_prices: list[HourlyPrice], price_area: str
+):
+    year, month = month_string.split("-")
+    output = list()
+    for price in hourly_prices:
+        output.append(file_converter.unstructure(price))
+    try:
+        os.makedirs(f"{BASE_DIRECTORY}/{price_area}/{year}/{month}")
+    except OSError:
+        # folders exists
+        pass
+    with open(f"{BASE_DIRECTORY}/{price_area}/{year}/{month}/index.json", "w") as file:
+        json.dump(output, file)
 
-def group_hourly_values_by(
+def save_year_to_file(
+    year_string: str, hourly_prices: list[HourlyPrice], price_area: str
+):
+    output = list()
+    for price in hourly_prices:
+        output.append(file_converter.unstructure(price))
+    try:
+        os.makedirs(f"{BASE_DIRECTORY}/{price_area}/{year_string}")
+    except OSError:
+        # folders exists
+        pass
+    with open(f"{BASE_DIRECTORY}/{price_area}/{year_string}/index.json", "w") as file:
+        json.dump(output, file)
+
+def group_hourly_values_by_day(
     hourly_prices: list[HourlyPrice],
 ) -> dict[str, list[HourlyPrice]]:
     out = dict()
@@ -87,9 +115,33 @@ def group_hourly_values_by(
             out[price.timestamp.strftime("%Y-%m-%d")].append(price)
     return out
 
+def group_hourly_values_by_month(
+    hourly_prices: list[HourlyPrice],
+) -> dict[str, list[HourlyPrice]]:
+    out = dict()
+    for price in hourly_prices:
+        try:
+            out[price.timestamp.strftime("%Y-%m")].append(price)
+        except KeyError:
+            out[price.timestamp.strftime("%Y-%m")] = list()
+            out[price.timestamp.strftime("%Y-%m")].append(price)
+    return out
 
-def get_saved_prices_for_day(day_string: str, price_area: str) -> list[HourlyPrice]:
-    year, month, day = day_string.split("-")
+def group_hourly_values_by_year(
+    hourly_prices: list[HourlyPrice],
+) -> dict[str, list[HourlyPrice]]:
+    out = dict()
+    for price in hourly_prices:
+        try:
+            out[price.timestamp.strftime("%Y")].append(price)
+        except KeyError:
+            out[price.timestamp.strftime("%Y")] = list()
+            out[price.timestamp.strftime("%Y")].append(price)
+    return out
+
+
+def get_saved_prices_for_day(date_string: str, price_area: str) -> list[HourlyPrice]:
+    year, month, day = date_string.split("-")
     try:
         with open(f"{BASE_DIRECTORY}/{price_area}/{year}/{month}/{day}/index.json", "r") as file:
             out = list()
@@ -101,6 +153,35 @@ def get_saved_prices_for_day(day_string: str, price_area: str) -> list[HourlyPri
 
     except FileNotFoundError:
         return list()
+
+
+def get_saved_prices_for_month(month_string: str, price_area: str) -> list[HourlyPrice]:
+    year, month = month_string.split("-")
+    try:
+        with open(f"{BASE_DIRECTORY}/{price_area}/{year}/{month}/index.json", "r") as file:
+            out = list()
+            data = json.load(file)
+            for item in data:
+                out.append(file_converter.structure(item, HourlyPrice))
+
+            return out
+
+    except FileNotFoundError:
+        return list()
+
+def get_saved_prices_for_year(year_string: str, price_area: str) -> list[HourlyPrice]:
+    try:
+        with open(f"{BASE_DIRECTORY}/{price_area}/{year_string}/index.json", "r") as file:
+            out = list()
+            data = json.load(file)
+            for item in data:
+                out.append(file_converter.structure(item, HourlyPrice))
+
+            return out
+
+    except FileNotFoundError:
+        return list()
+
 
 
 def merge_prices(
@@ -117,6 +198,7 @@ def get_latest_price_area_data(price_area: str, days_back: int, days_ahead: int)
     from_date = today - timedelta(days=days_back)
     to_date = today + timedelta(days=days_ahead)
     url = url_of_price_area(price_area, from_date, to_date)
+
     response = httpx.get(url, timeout=20)
     data = response.json()
 
@@ -144,19 +226,32 @@ def save_latest_prices(latest_prices: list[HourlyPrice], price_area: str):
 if __name__ == "__main__":
     for area in PRICE_AREAS:
         hourly_prices = get_latest_price_area_data(area, 4, 1)
-        grouped_prices = group_hourly_values_by(hourly_prices)
+        grouped_prices_by_day = group_hourly_values_by_day(hourly_prices)
+        grouped_prices_by_month = group_hourly_values_by_month(hourly_prices)
+        grouped_prices_by_year = group_hourly_values_by_year(hourly_prices)
 
-        for day in grouped_prices.keys():
+
+        for day in grouped_prices_by_day.keys():
             saved_prices = get_saved_prices_for_day(day, area)
-            merged_prices = merge_prices(saved_prices, grouped_prices[day])
+            merged_prices = merge_prices(saved_prices, grouped_prices_by_day[day])
             save_day_to_file(day, merged_prices, area)
+
+        for month in grouped_prices_by_month.keys():
+            saved_prices = get_saved_prices_for_month(month, area)
+            merged_prices = merge_prices(saved_prices, grouped_prices_by_month[month])
+            save_month_to_file(month, merged_prices, area)
+
+        for year in grouped_prices_by_year.keys():
+            saved_prices = get_saved_prices_for_year(year, area)
+            merged_prices = merge_prices(saved_prices, grouped_prices_by_year[year])
+            save_year_to_file(year, merged_prices, area)
 
         today = datetime.now(tz=zoneinfo.ZoneInfo("ETC/GMT-1")).date()
         tomorrow = today + timedelta(days=1)
         latest_prices = list()
-        latest_prices.extend(grouped_prices[today.strftime("%Y-%m-%d")])
+        latest_prices.extend(grouped_prices_by_day[today.strftime("%Y-%m-%d")])
         try:
-            latest_prices.extend(grouped_prices[tomorrow.strftime("%Y-%m-%d")])
+            latest_prices.extend(grouped_prices_by_day[tomorrow.strftime("%Y-%m-%d")])
         except KeyError:
             # Before they are pubished we wont get the data
             pass
